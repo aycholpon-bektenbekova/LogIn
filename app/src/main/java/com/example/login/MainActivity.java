ebc10915ea663896de612d2506d6b5ca334c042a
DELL <aycholpon25t@gmail.com>
2022-09-22 22:12:38 +0600

    Initial commit

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        EditText editText = findViewById(R.id.edittext1);
+
+        Toast.makeText(MainActivity.this, editText.getText().toString(),
+                Toast.LENGTH_SHORT).show();
+    }
+}
